9a229308e9f47d3b0e066cd56fe58e378eabc0a4
Gaetan <gaetan@protonmail.com>
2020-01-24 13:21:48 +0100

    create the folders envar and env simple and gitignore

---
+++ envar.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config('.env')
+
+process.env.MY_NAME;
+process.env.MY_CITY;
+process.env.MY_LANGUAGE;
+
+console.log(`I am ${process.env.MY_NAME} and i live in ${process.env.MY_CITY} so i'm wilder in ${process.env.MY_CITY} 
+and i love  ${process.env.MY_LANGUAGE}`);
+
+
+ 
